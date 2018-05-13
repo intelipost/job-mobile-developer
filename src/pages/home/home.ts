7eb41326ded37945a228ed3d6e7f28d2e319f127
Maria Luiza Fornagieri <mluiza_fornagieri@outlook.com>
2018-05-12 23:27:27 -0300

    layout e estilização inicial das páginas

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { LocationsPage } from '../locations/locations';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -9,6 +11,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  locationsPage() {
+    this.navCtrl.push(LocationsPage);
   }
 
 }
