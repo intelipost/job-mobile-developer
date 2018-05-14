c300cb324625c688618e1342739f8f8c12ed02d6
Maria Luiza Fornagieri <mluiza_fornagieri@outlook.com>
2018-05-14 02:43:40 -0300

    removido tabela

--- src/providers/local-db/local-db.ts
+++ src/providers/local-db/local-db.ts
@@ -15,8 +15,8 @@ export class LocalDbProvider {
   }
 
   //  Usando SQLite como solução para a parte de armazenamento local, primeiramente abre-se ou cria-se o banco 
-  //  usando a função abaixo, então o objeto do bd é passado como argumento para criar as tabelas se as mesmas
-  //  não existirem.
+  //  usando a função abaixo, então o objeto do bd é passado como argumento para criar a tabela se a mesma
+  //  não existir.
 
   getDb() {
     return this.sqlite.create({
@@ -25,17 +25,14 @@ export class LocalDbProvider {
     })
   }
 
-  // Cria duas tabelas (locations e locations_synced) se as duas não já existirem no banco. 
+  // Cria tabela (locations) se ela não já existir no banco. 
   // locations é a tabela com as localizações que ainda não foram passadas para o servidor,
-  // locations_synced é a tabela com as localizações que já foram sincronizadas para o server.
 
   createTables() {
     return this.getDb()
       .then((db: SQLiteObject) => { 
       db.executeSql("create table if not exists locations(id INTEGER PRIMARY KEY AUTOINCREMENT, time TEXT, lng TEXT, lat TEXT)", {})
         .catch(err => console.log('Erro na criação da tabela locations:', err));
-        db.executeSql("create table if not exists locations_synced(id INTEGER PRIMARY KEY AUTOINCREMENT, time TEXT, lng TEXT, lat TEXT)", {})
-        .catch(err => console.log('Erro na criação da tabela locations_synced:', err));
     })
   }
 
