df7ee0e1613da36ef0d0ef4fd292eac9dd885574
Willian Custódio <wrafhael@gmail.comgit statusexitgit config --global user.email wrafhael@gmail.com>
2018-07-22 10:24:18 -0300

    Saving location on local database

--- src/providers/location.ts
+++ src/providers/location.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Platform } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
+import { DatabaseProvider } from './database';
+import { LocationStrategy } from '../../node_modules/@angular/common';
 
 const LOCATION_INTERVAL = 180000;
 
@@ -9,12 +11,23 @@ const LOCATION_INTERVAL = 180000;
 export class LocationProvider {
     private intervalId: any;
 
-    constructor(private platform: Platform, private geolocation: Geolocation) {
+    constructor(
+        private platform: Platform, 
+        private geolocation: Geolocation,
+        private database: DatabaseProvider
+    ) {
     }
 
     startWatchPosition() {
         this.getLocation().subscribe(res => {
+            //armazena a primeira localização
+            this.saveLocation(res);
+
+            //inicia o processo para capturar a localização a cada 3 minutos
             this.intervalId = setInterval(() => {
+                this.getLocation().subscribe(res => {
+                    this.saveLocation(res);
+                })
             }, LOCATION_INTERVAL)
         })
     }
@@ -22,10 +35,19 @@ export class LocationProvider {
     stopWatchPosition() {
         clearInterval(this.intervalId);
     }
+
+    saveLocation(location) {
+        this.database.executeSql('INSERT INTO locations(date, latitude, longitude, sync) VALUES(?,?,?,?)', [location.date, location.latitude, location.longitude, false]).subscribe(res => {
+            console.log('coordenadas armazenada');
+        }, err => {
+            console.error('falha ao salvar a localização', err);
+        })
+    }
     
     getLocation() {
         return Observable.create(observer => {
             if (this.platform.is('cordova')) {
+                //obtém a localização com modo de alta precisão
                 this.geolocation.getCurrentPosition({
                     timeout: 6000,
                     enableHighAccuracy: true
@@ -41,6 +63,7 @@ export class LocationProvider {
                     observer.error(err);
                 });             
             } else {
+                //localização fake para rodar fora do device no ambiente de teste
                 observer.next({
                     longitude: -3.60512,
                     latitude: 55.070859,
